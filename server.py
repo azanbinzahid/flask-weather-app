a2b95ef28f4bf79e4c28732df1319a1a1a491393
azanbinzahid <azanbinzahid@gmail.com>
2020-03-26 15:01:45 +0500

    basic CRUD operations implemented like ADD and Delete

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pyrebase
-from config import firebaseAPI
+import requests
+from config import firebaseAPI, openweathermapAPI
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -15,6 +16,7 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+cityweather= "http://api.openweathermap.org/data/2.5/weather?q={}&app_id=" + openweathermapAPI
 
 # init db
 # data = {
@@ -27,8 +29,13 @@ db = firebase.database()
 # db.set(data)
 
 
-@app.route("/", methods =["GET", "POST"])
+@app.route("/")
 def index():
+    cities = db.child("cities").get().val()
+    return render_template("index.html", cities = cities)
+
+@app.route("/addcity", methods =["GET", "POST"])
+def addCity():
     if request.method=="POST":
         name = request.form["name"]
         if name:
@@ -36,10 +43,25 @@ def index():
                 "name": name
             }
             db.child("cities").push(data)
+    return redirect(url_for('index'))
 
+@app.route("/weather")
+def getWeather():
     cities = db.child("cities").get().val()
-    return render_template("index.html", cities = cities)
 
+    allData = []
+    for city in cities.values():
+        data = requests.get(cityweather.format(city['name'])).json()
+        allData.append(data)
+    
+    print (allData)
+    return "done"
+
+@app.route("/deletecity", methods=['POST'])
+def deleteCity():
+    city_id = request.form['city_id']
+    db.child("cities").child(city_id).remove()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
